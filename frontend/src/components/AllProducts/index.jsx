2587e671b6054fd7eec805333cb84c4f4301f7a5
Erick William <erikcwill@gmail.com>
2023-09-28 03:08:44 -0300

    updateDB

--- frontend/src/components/AllProducts/index.jsx
+++ frontend/src/components/AllProducts/index.jsx
@@ -1,15 +1,23 @@
 import  { useEffect, useState } from "react";
+import ProductService from "../ProductService";
 import ProductItem from "../ProductItem";
-import { getAllProducts, toggleProductHighlight } from "../ProductService";
 
 export default function AllProducts() {
   const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    category: "",
+    image: "",
+    description: "",
+    isHighlighted: false,
+  });
 
   useEffect(() => {
     async function fetchAllProducts() {
       try {
-        const productsData = await getAllProducts();
-        setProducts(productsData);
+        const data = await ProductService.getAllProducts();
+        setProducts(data);
       } catch (error) {
         console.error("Erro ao buscar produtos:", error);
       }
@@ -18,19 +26,44 @@ export default function AllProducts() {
     fetchAllProducts();
   }, []);
 
-  async function handleToggleHighlight(product) {
-    try {
-      await toggleProductHighlight(product.id, !product.isHighlighted);
-      // Atualize a lista de produtos após a atualização
+  const updateProductList = (updatedProduct, removedProductId) => {
+    if (removedProductId) {
       setProducts((prevProducts) =>
-        prevProducts.map((p) =>
-          p.id === product.id ? { ...p, isHighlighted: !p.isHighlighted } : p
+        prevProducts.filter((product) => product.id !== removedProductId)
+      );
+    } else if (updatedProduct) {
+      setProducts((prevProducts) =>
+        prevProducts.map((product) =>
+          product.id === updatedProduct.id ? updatedProduct : product
         )
       );
-    } catch (error) {
-      console.error("Erro ao atualizar status de destaque:", error);
     }
-  }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({
+      ...newProduct,
+      [name]: value,
+    });
+  };
+
+  const addNewProduct = async () => {
+    try {
+      const response = await ProductService.addProduct(newProduct);
+      setNewProduct({
+        name: "",
+        price: "",
+        category: "",
+        image: "",
+        description: "",
+        isHighlighted: false,
+      });
+      setProducts([...products, response]);
+    } catch (error) {
+      console.error("Erro ao adicionar produto:", error);
+    }
+  };
 
   return (
     <div>
@@ -40,10 +73,71 @@ export default function AllProducts() {
           <ProductItem
             key={product.id}
             product={product}
-            onToggleHighlight={handleToggleHighlight}
+            onUpdate={updateProductList}
           />
         ))}
       </ul>
+      <h2>Adicionar Novo Produto</h2>
+      <div>
+        <label>Nome:</label>
+        <input
+          type="text"
+          name="name"
+          value={newProduct.name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Preço:</label>
+        <input
+          type="number"
+          name="price"
+          value={newProduct.price}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Categoria:</label>
+        <input
+          type="text"
+          name="category"
+          value={newProduct.category}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Imagem:</label>
+        <input
+          type="text"
+          name="image"
+          value={newProduct.image}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Descrição:</label>
+        <input
+          type="text"
+          name="description"
+          value={newProduct.description}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Destacado:</label>
+        <input
+          type="checkbox"
+          name="isHighlighted"
+          checked={newProduct.isHighlighted}
+          onChange={() =>
+            setNewProduct({
+              ...newProduct,
+              isHighlighted: !newProduct.isHighlighted,
+            })
+          }
+        />
+      </div>
+      <button onClick={addNewProduct}>Adicionar Produto</button>
     </div>
   );
 }
